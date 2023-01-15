8b8f5d51daf94e5af728f86dbe0a60e60185c973
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-15 21:09:46 +0100

    Paso 12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         $this->call(SubcategorySeeder::class);
         $this->call(ProductSeeder::class);
+        $this->call(ColorSeeder::class);
+        $this->call(ColorProductSeeder::class);
     }
 }
