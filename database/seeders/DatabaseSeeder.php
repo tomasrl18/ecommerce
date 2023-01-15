1626fcfdb44637943ce819f6733886779d3d594a
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-15 21:12:40 +0100

    Paso 13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeeder::class);
         $this->call(ColorSeeder::class);
         $this->call(ColorProductSeeder::class);
+        $this->call(SizeSeeder::class);
     }
 }
--- database/seeders/SizeSeeder.php
+++ database/seeders/SizeSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Seeder;
 
 class SizeSeeder extends Seeder
@@ -13,6 +15,19 @@ class SizeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = Product::whereHas('subcategory', function(Builder $query){
+            $query->where('color', true)
+                ->where('size', true);
+        })->get();
+
+        $sizes = ['Talla S', 'Talla M', 'Talla L'];
+
+        foreach ($products as $product) {
+            foreach ($sizes as $size) {
+                $product->sizes()->create([
+                    'name' => $size
+                ]);
+            }
+        }
     }
 }
