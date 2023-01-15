f0bf6c40bab36a4206040a8f9b4abdae2feac745
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-15 19:45:13 +0100

    Paso 6

---
+++ database/migrations/2023_01_15_183717_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->float('price');
+            $table->foreignId('subcategory_id')->references('id')->on('subcategories');
+            $table->foreignId('brand_id')->references('id')->on('brands');
+            $table->integer('quantity')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
