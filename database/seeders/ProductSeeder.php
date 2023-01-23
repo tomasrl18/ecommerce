ea2bb0e9e0a201534ad80db2788cfac5477f0196
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-23 22:21:06 +0100

    Paso 38

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,7 +16,7 @@ class ProductSeeder extends Seeder
     public function run()
     {
         // Esto debería de estar a 250, lo he puesto a 50 por el bien de mi tiempo
-        Product::factory(50)->create()
+        Product::factory(100)->create()
             ->each(function(Product $product){
                 Image::factory(4)->create([
                     'imageable_id' => $product->id,
