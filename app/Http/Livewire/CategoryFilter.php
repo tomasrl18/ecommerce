94bc72ea05d5818534226e27da2870422f6a39ce
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-24 09:17:24 +0100

    Paso 40

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,10 +15,26 @@ class CategoryFilter extends Component
 
     public function render()
     {
-        $products = $this->category
-                ->products()
-                ->where('status', 2)
-                ->paginate(20);
+        $productsQuery = Product::query()
+            ->whereHas('subcategory.category', function(Builder $query) {
+            $query->where('id', $this->category->id);
+        });
+
+        if ($this->subcategoria) {
+            $productsQuery = $productsQuery
+                ->whereHas('subcategory', function(Builder $query) {
+                $query->where('name', $this->subcategoria);
+            });
+        }
+
+        if ($this->marca) {
+            $productsQuery = $productsQuery
+                ->whereHas('brand', function(Builder $query) {
+                $query->where('name', $this->marca);
+            });
+        }
+
+        $products = $productsQuery->paginate(20);
 
         return view('livewire.category-filter', compact('products'));
     }
