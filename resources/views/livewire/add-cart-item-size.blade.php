3cffb79a6f324db6a1319b90a0503f4a2908b085
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-24 11:40:43 +0100

    Paso 52

--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div>
+        <p class="text-xl text-gray-700">Talla:</p>
+
+        <select wire:model="size_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione una talla</option>
+            @foreach ($sizes as $size)
+                <option value="{{$size->id}}">{{$size->name}}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="mt-2">
+        <p class="text-xl text-gray-700">Color:</p>
+
+        <select class="form-control w-full">
+            <option value="" selected disabled>Seleccione un color</option>
+            @foreach ($colors as $color)
+                <option value="{{$color->id}}">{{$color->name}}</option>
+            @endforeach
+        </select>
+    </div>
 </div>
