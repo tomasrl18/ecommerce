840b93de7e9b44f430ba7eade0210f15453d0985
tomasrl18 <tomaslopezraigal@gmail.com>
2023-01-24 09:29:58 +0100

    Paso 42

--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-5 gap-6">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         <aside>
             <h2 class="font-semibold text-center mb-2">Subcategorías</h2>
 
@@ -45,9 +45,9 @@
             </x-jet-button>
         </aside>
 
-        <div class="col-span-4">
+        <div class="md:col-span-2 lg:col-span-4">
             @if($view == 'grid')
-                <ul class="grid grid-cols-4 gap-6">
+                <ul class="grid grid-cols-1 lg:grid-cols-4 gap-6">
                     @foreach($products as $product)
                         <li class="bg-white rounded-lg shadow">
                             <article>
