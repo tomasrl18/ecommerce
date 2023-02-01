d2154dfb14b90577de62745d2e8b662063dd4f0c
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-01 09:50:38 +0100

    Paso 83

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Listeners\MergeTheCart;
+use App\Listeners\MergeTheCartLogout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +21,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Login::class => [
+            MergeTheCart::class,
+        ],
+        Logout::class => [
+            MergeTheCartLogout::class,
         ],
     ];
 
