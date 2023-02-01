6f4e14c7035adc3923970132497c11dca387af7b
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-01 19:01:26 +0100

    Paso 91

--- resources/views/livewire/create-order.blade.php
+++ resources/views/livewire/create-order.blade.php
@@ -23,86 +23,133 @@
                 <span class="font-semibold text-gray-700 ml-auto">Gratis</span>
             </label>
 
-            <label class="bg-white rounded-lg shadow px-6 py-4 flex items-center">
-                <input type="radio" name="envio" class="text-gray-600">
-                <span class="ml-2 text-gray-700">Envío a domicilio</span>
-            </label>
-        </div>
+            <div class="bg-white rounded-lg shadow">
+                <label class="px-6 py-4 flex items-center">
+                    <input type="radio" name="envio" class="text-gray-600">
+                    <span class="ml-2 text-gray-700">Envío a domicilio</span>
+                </label>
 
-        <div>
-            <x-jet-button class="mt-6 mb-4">
-                Continuar con la compra
-            </x-jet-button>
+                <div class="px-6 pb-6 grid grid-cols-2 gap-6">
+                    <div>
+                        <x-jet-label value="Departamento" />
 
-            <hr>
+                        <select class="form-control w-full" wire:model="department_id">
+                            <option value="" disabled selected>Seleccione un departamento</option>
+                            @foreach($departments as $department)
+                                <option value="{{ $department->id }}">{{ $department->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
-            <p class="text-sm text-gray-700 mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, maiores,
-                porro. Accusantium architecto cum excepturi necessitatibus omnis ratione, rerum sed similique veniam. Dolorum iste, omnis
-                repudiandae sunt tempora totam unde!
+                    <div>
+                        <x-jet-label value="Ciudad" />
 
-                <a href="" class="font-semibold text-orange-500">
-                    Políticas y privacidad
-                </a>
-            </p>
-        </div>
-    </div>
+                        <select class="form-control w-full" wire:model="city_id">
+                            <option value="" disabled selected>Seleccione una ciudad</option>
 
-    <div class="col-span-2">
-        <div class="bg-white rounded-lg shadow p-6">
-            <ul>
-                @forelse(Cart::content() as $item)
-                    <li class="flex p-2 border-b border-gray-200">
-                        <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                            @foreach($cities as $city)
+                                <option value="{{ $city->id }}">{{ $city->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
-                        <article class="flex-1">
-                            <h1 class="font-bold">{{ $item->name }}</h1>
+                    <div>
+                        <x-jet-label value="Distrito" />
 
-                            <div class="flex">
-                                <p class="">Cant: {{ $item->qty }}</p>
+                        <select class="form-control w-full" wire:model="district_id">
+                            <option value="" disabled selected>Seleccione un distrito</option>
+                            @foreach($districts as $district)
+                                <option value="{{ $district->id }}">{{ $district->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
 
-                                @isset($item->options['color'])
-                                    <p class="mx-2">- Color: {{ __(ucfirst($item->options['color'])) }}</p>
-                                @endisset
+                <div>
+                    <x-jet-label value="Dirección" />
+                    <x-jet-input class="w-full" wire:model="address" type="text" />
+                </div>
 
-                                @isset($item->options['size'])
-                                    <p class="mx-2">{{ $item->options['size'] }}</p>
-                                @endisset
-                            </div>
+                <div class="col-span-2">
+                    <x-jet-label value="Referencia" />
+                    <x-jet-input class="w-full" wire:model="reference" type="text" />
+                </div>
+            </div>
 
-                            <p>{{ $item->price }} &euro;</p>
-                        </article>
-                    </li>
+            <div>
+                <x-jet-button class="mt-6 mb-4">
+                    Continuar con la compra
+                </x-jet-button>
 
-                @empty
+                <hr>
 
-                    <li class="py-6 px-4">
-                        <p class="text-center text-gray-700">
-                            No tiene agregado ningún item en el carrito
-                        </p>
-                    </li>
-                @endforelse
-            </ul>
+                <p class="text-sm text-gray-700 mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, maiores,
+                    porro. Accusantium architecto cum excepturi necessitatibus omnis ratione, rerum sed similique veniam. Dolorum iste, omnis
+                    repudiandae sunt tempora totam unde!
 
-            <hr class="mt-4 mb-3">
-
-            <div class="text-gray-700">
-                <p class="flex justify-between items-center">
-                    Subtotal
-                    <span>{{ Cart::subtotal() }} &euro;</span>
-                </p>
-
-                <p class="flex justify-between items-center">
-                    Envío
-                    <span class="font-semibold">Gratis</span>
-                </p>
-
-                <hr class="mt-4 mb-3">
-
-                <p class="flex justify-between items-center font-semibold">
-                    <span class="text-lg">Total</span>
-                    {{ Cart::subtotal() }} &euro;
+                    <a href="" class="font-semibold text-orange-500">
+                        Políticas y privacidad
+                    </a>
                 </p>
             </div>
         </div>
-    </div>
+
+        <div class="col-span-2">
+            <div class="bg-white rounded-lg shadow p-6">
+                <ul>
+                    @forelse(Cart::content() as $item)
+                        <li class="flex p-2 border-b border-gray-200">
+                            <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+
+                            <article class="flex-1">
+                                <h1 class="font-bold">{{ $item->name }}</h1>
+
+                                <div class="flex">
+                                    <p class="">Cant: {{ $item->qty }}</p>
+
+                                    @isset($item->options['color'])
+                                        <p class="mx-2">- Color: {{ __(ucfirst($item->options['color'])) }}</p>
+                                    @endisset
+
+                                    @isset($item->options['size'])
+                                        <p class="mx-2">{{ $item->options['size'] }}</p>
+                                    @endisset
+                                </div>
+
+                                <p>{{ $item->price }} &euro;</p>
+                            </article>
+                        </li>
+
+                    @empty
+
+                        <li class="py-6 px-4">
+                            <p class="text-center text-gray-700">
+                                No tiene agregado ningún item en el carrito
+                            </p>
+                        </li>
+                    @endforelse
+                </ul>
+
+                <hr class="mt-4 mb-3">
+
+                <div class="text-gray-700">
+                    <p class="flex justify-between items-center">
+                        Subtotal
+                        <span>{{ Cart::subtotal() }} &euro;</span>
+                    </p>
+
+                    <p class="flex justify-between items-center">
+                        Envío
+                        <span class="font-semibold">Gratis</span>
+                    </p>
+
+                    <hr class="mt-4 mb-3">
+
+                    <p class="flex justify-between items-center font-semibold">
+                        <span class="text-lg">Total</span>
+                        {{ Cart::subtotal() }} &euro;
+                    </p>
+                </div>
+            </div>
+        </div>
 </div>
