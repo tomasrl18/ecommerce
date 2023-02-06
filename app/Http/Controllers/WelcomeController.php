d328cd561cc08c7416a998b1bfbab6105588455b
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-06 11:17:13 +0100

    Paso 116

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Order;
 
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
+        if (auth()->user()) {
+            $pendientes = Order::where('user_id', auth()->user()->id)->where('status', 1)->count();
+            if ($pendientes) {
+                $mensaje = "Tiene $pendientes ordenes pendientes de pago. <a class='font-bold' href='" . route('orders.index') .
+                    "?status=1'>Pagar</a>";
+                session()->flash('flash.banner', $mensaje);
+            }
+        }
+
         $categories = Category::all();
 
         return view('welcome', compact('categories'));
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -41,10 +41,16 @@
             </div>
         </div>
 
-        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6 flex items-center">
             <p class="text-gray-700 uppercase"><span class="font-semibold">Número de Orden:</span>
                 {{ $order->id }}
             </p>
+
+            @if ($order->status == 1)
+                <x-button-link class="ml-auto" href="{{route('orders.payment', $order)}}">
+                    Ir a pagar
+                </x-button-link>
+            @endif
         </div>
 
         <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
