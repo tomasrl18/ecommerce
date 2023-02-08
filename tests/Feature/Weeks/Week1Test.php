ecf0a8e478e9ed394533396df7ee9189848274e6
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-08 21:18:22 +0100

    S1. T1 sin hacer.

---
+++ tests/Feature/Weeks/Week1Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Weeks;
+
+use App\Http\Livewire\Navigation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class Week1Test extends TestCase
+{
+    /** @test */
+    public function ()
+    {
+
+
+//        Livewire::test(Navigation::class)
+//            ->
+    }
+}
