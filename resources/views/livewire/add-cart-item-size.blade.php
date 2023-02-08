83988a589e0e9123f4cdaaec433acea02284d5c1
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-08 13:31:21 +0100

    Solución error stock color y talla

--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -22,7 +22,13 @@
     </div>
 
     <p class="text-gray-700 my-4">
-        <span class="font-semibold text-lg">Stock disponible:</span> {{ $product->stock }}
+        <span class="font-semibold text-lg">Stock disponible:</span>
+
+        @if($quantity)
+            {{ $quantity }}
+        @else
+            {{ $product->stock }}
+        @endif
     </p>
 
     <div class="flex">
