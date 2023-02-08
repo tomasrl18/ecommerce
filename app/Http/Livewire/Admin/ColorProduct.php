8ba1fdf6b4d4e8163c8ac369f9ecc982bf983982
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-08 12:58:49 +0100

    Paso 136

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Color;
 use Livewire\Component;
 
 class ColorProduct extends Component
 {
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+
+    public $product, $colors;
+    public $color_id, $quantity;
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
     public function render()
     {
         return view('livewire.admin.color-product');
+    }
+
+    public function save(){
+        $this->validate();
+
+        $this->product->colors()->attach([
+            $this->color_id => [
+                'quantity' => $this->quantity
+            ]
+        ]);
+
+        $this->reset(['color_id', 'quantity']);
+        $this->emit('saved');
     }
 }
--- resources/views/livewire/admin/color-product.blade.php
+++ resources/views/livewire/admin/color-product.blade.php
@@ -1,3 +1,40 @@
 <div>
-    Necesita especificar un color
+    <div class="my-12 bg-white shadow-lg rounded-lg p-6">
+        <div class="mb-6">
+            <x-jet-label>
+                Color
+            </x-jet-label>
+
+            <div class="grid grid-cols-6 gap-6">
+                @foreach ($colors as $color)
+                    <label>
+                        <input type="radio" name="color_id" wire:model.defer="color_id" value="{{ $color->id }}">
+
+                        <span class="ml-2 text-gray-700 capitalize">
+                            {{ __(ucfirst($color->name)) }}
+                        </span>
+                    </label>
+                @endforeach
+            </div>
+
+            <x-jet-input-error for="color_id" />
+        </div>
+
+        <div>
+            <x-jet-label>
+                Cantidad
+            </x-jet-label>
+            <x-jet-input type="number" wire:model.defer="quantity" placeholder="Ingrese una cantidad" class="w-full" />
+            <x-jet-input-error for="quantity" />
+        </div>
+
+        <div class="flex justify-end items-center mt-4">
+            <x-jet-action-message class="mr-3" on="saved">
+                Agregado
+            </x-jet-action-message>
+            <x-jet-button wire:loading.attr="disabled" wire:target="save" wire:click="save">
+                Agregar
+            </x-jet-button>
+        </div>
+    </div>
 </div>
