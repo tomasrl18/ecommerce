6cf3f54dd2539f147c797b4b10516346a3b0ae0f
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-13 23:42:57 +0100

    Paso 154

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+
+class ProductObserver
+{
+    public function updated(Product $product)
+    {
+        $subcategory = $product->subcategory;
+
+        if ($subcategory->size) {
+            $product->colors()->detach();
+        } elseif ($subcategory->color) {
+            foreach ($product->sizes as $size) {
+                $size->delete();
+            }
+        } else {
+            $product->colors()->detach();
+
+            foreach ($product->sizes as $size) {
+                $size->delete();
+            }
+        }
+    }
+}
