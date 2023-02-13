0dc4f3e12a11f5764e0301ca5c5048ebdb65dec9
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-13 19:43:07 +0100

    Paso 143

--- app/Http/Livewire/Admin/SizeProduct.php
+++ app/Http/Livewire/Admin/SizeProduct.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Size;
 use Livewire\Component;
 
 class SizeProduct extends Component
 {
-    public $product, $name;
+    public $product, $name, $size;
+    public $open = false;
+    public $name_edit;
 
     protected $rules = [
         'name' => 'required'
@@ -21,6 +24,27 @@ class SizeProduct extends Component
         ]);
 
         $this->product = $this->product->fresh();
+    }
+
+    public function edit(Size $size)
+    {
+        $this->open = true;
+        $this->size = $size;
+        $this->name_edit = $size->name;
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'name_edit' => 'required',
+        ]);
+
+        $this->size->name = $this->name_edit;
+        $this->size->save();
+
+        $this->product = $this->product->fresh();
+
+        $this->open = false;
     }
 
     public function render()
--- resources/views/livewire/admin/size-product.blade.php
+++ resources/views/livewire/admin/size-product.blade.php
@@ -24,9 +24,48 @@
         @foreach ($sizes as $size)
             <li class="bg-white shadow-lg rounded-lg p-6" wire:key="size-{{ $size->id }}">
                 <div class="flex items-center">
-                    <span class="text-xl font-medium">{{ $size->name }}</span>
+                    <span class="text-xl font-medium">
+                        {{ $size->name }}
+                    </span>
+
+                    <div class="ml-auto">
+                        <x-jet-button wire:click="edit({{ $size->id }})" wire:loading.attr="disabled"
+                                      wire:target="edit({{ $size->id }})">
+                            <i class="fas fa-edit"></i>
+                        </x-jet-button>
+
+                        <x-jet-danger-button>
+                            <i class="fas fa-trash"></i>
+                        </x-jet-danger-button>
+                    </div>
                 </div>
             </li>
         @endforeach
     </ul>
+
+    <x-jet-dialog-modal wire:model="open">
+        <x-slot name="title">
+            Editar talla
+        </x-slot>
+
+        <x-slot name="content">
+            <x-jet-label>
+                Talla
+            </x-jet-label>
+
+            <x-jet-input wire:model="name_edit" type="text" class="w-full" />
+
+            <x-jet-input-error for="name_edit" />
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$set('open', false)">
+                Cancelar
+            </x-jet-secondary-button>
+
+            <x-jet-button wire:click="update" wire:loading.attr="disabled" wire:target="update">
+                Actualizar
+            </x-jet-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
