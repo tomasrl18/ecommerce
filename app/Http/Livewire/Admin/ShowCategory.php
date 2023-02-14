8060ff2816396275a102f0f225478f599a6c776b
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-15 00:29:46 +0100

    Paso 169

--- app/Http/Livewire/Admin/ShowCategory.php
+++ app/Http/Livewire/Admin/ShowCategory.php
@@ -3,15 +3,59 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class ShowCategory extends Component
 {
-    public $category;
+    public $category, $subcategory;
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'color' => false,
+        'size' => false,
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:subcategories,slug',
+        'createForm.color' => 'required',
+        'createForm.size' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.color' => 'color',
+        'createForm.size' => 'talla',
+    ];
 
     public function mount(Category $category)
     {
         $this->category = $category;
+        $this->getSubcategories();
+    }
+
+    public function save()
+    {
+        $this->validate();
+    }
+
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function getSubcategories()
+    {
+        $this->subcategories = Subcategory::where('category_id', $this->category->id)->get();
+    }
+
+    public function edit(Subcategory $subcategory)
+    {
+
     }
 
     public function render()
