223957bcbe53a495e4b123e479164f63a1306ceb
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-15 10:29:07 +0100

    Paso 190

--- resources/views/livewire/payment-order.blade.php
+++ resources/views/livewire/payment-order.blade.php
@@ -1,5 +1,5 @@
-<div class="grid grid-cols-5 gap-6 container-menu py-12">
-    <div class="col-span-3">
+<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-5 gap-6 container-menu py-8">
+    <div class="order-2 lg:order-1 xl:col-span-3">
         <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
             <p class="text-gray-700 uppercase"><span class="font-semibold">
                         Número de Orden:
@@ -86,7 +86,7 @@
         </div>
     </div>
 
-    <div class="col-span-2">
+    <div class="order-1 lg:order-2 xl:col-span-2">
         <div class="bg-white rounded-lg shadow-lg px-6 pt-6">
             <div class="flex justify-between items-center mb-4">
                 <img class="h-8" src="{{ asset('img/MC_VI_DI_2-1.jpg') }}" alt="">
