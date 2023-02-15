dc9b7f8a61571666b8db7406caddb483d32f23cb
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-15 10:01:49 +0100

    Paso 186

--- resources/views/components/products-list.blade.php
+++ resources/views/components/products-list.blade.php
@@ -1,13 +1,13 @@
 @props(['product'])
 
 <li class="bg-white rounded-lg shadow mb-4">
-    <article class="flex">
+    <article class="md:flex">
         <figure>
-            <img class="h-48 w-56 object-cover object-center" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+            <img class="h-48 w-full md:w-56 object-cover object-center" src="{{ Storage::url($product->images->first()->url) }}" alt="">
         </figure>
 
         <div class="flex-1 py-4 px-6 flex flex-col">
-            <div class="flex justify-between">
+            <div class="lg:flex justify-between">
 
                 <div>
                     <h1 class="text-lg font-semibold text-gray-700">{{ $product->name }}</h1>
--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -3,7 +3,7 @@
         <div class="px-6 py-2 flex justify-between items-center">
             <h1 class="font-semibold text-gray-700 uppercase">{{ $category->name }}</h1>
 
-            <div class="grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
+            <div class="hidden md:block grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
                 <i class="fas fa-border-all p-3 cursor-pointer {{ $view == 'grid' ? 'text-orange-500' : '' }}" wire:click="$set('view', 'grid')"></i>
                 <i class="fas fa-th-list p-3 cursor-pointer {{ $view == 'list' ? 'text-orange-500' : '' }}" wire:click="$set('view', 'list')"></i>
             </div>
