4f28e7ed37cce95ff501bab4d5740443ce2e7f5c
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-15 17:49:35 +0100

    Arreglado botón de eliminar de Productos

--- resources/views/livewire/admin/edit-product.blade.php
+++ resources/views/livewire/admin/edit-product.blade.php
@@ -183,7 +183,7 @@
                 },
                 queuecomplete: function() {
                     Livewire.emit('refreshProduct');
-                } // MB
+                }
             };
 
             Livewire.on('deleteSize', sizeId => {
@@ -248,6 +248,27 @@
                 }).then((result) => {
                     if (result.isConfirmed) {
                         Livewire.emitTo('admin.color-size', 'delete', pivot);
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+            })
+
+            Livewire.on('deleteProduct', () => {
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Livewire.emitTo('admin.edit-product', 'delete');
                         Swal.fire(
                             'Deleted!',
                             'Your file has been deleted.',
