416b58ce74cce53f1301d784c462e3de1ee05863
tomasrl18 <tomaslopezraigal@gmail.com>
2023-02-15 09:47:03 +0100

    Paso 183

--- app/Http/Livewire/Admin/UserComponent.php
+++ app/Http/Livewire/Admin/UserComponent.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UserComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.user-component')
+        $users = User::paginate();
+
+        return view('livewire.admin.user-component', compact('users'))
             ->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/user-component.blade.php
+++ resources/views/livewire/admin/user-component.blade.php
@@ -4,4 +4,87 @@
             Usuarios
         </h2>
     </x-slot>
+
+    <div class="container-menu py-12">
+        <x-table-responsive>
+            @if (count($users))
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                ID
+                            </th>
+
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Nombre
+                            </th>
+
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Email
+                            </th>
+
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Rol
+                            </th>
+
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Editar</span>
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach ($users as $user)
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-gray-900">
+                                        {{$user->id}}
+                                    </div>
+                                </td>
+
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        {{$user->name}}
+                                    </div>
+                                </td>
+
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">
+                                        {{$user->email}}
+                                    </div>
+                                </td>
+
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <div class="text-sm text-gray-900">
+                                        @if ($user->roles->count())
+                                            Admin
+                                        @else
+                                            No tiene rol
+                                        @endif
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div class="px-6 py-4">
+                    No hay ningún registro coincidente
+                </div>
+            @endif
+
+            @if ($users->hasPages())
+                <div class="px-6 py-4">
+                    {{$users->links()}}
+                </div>
+            @endif
+        </x-table-responsive>
+    </div>
 </div>
